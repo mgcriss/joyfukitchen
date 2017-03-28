df59b3511c4f0a8b2eb2eb60f0a0d9211569c322
535727655 <2877143504@qq.com>
2017-03-28 16:17:48 +0800

    add package constants

---
+++ app/src/main/java/edu/ayd/joyfukitchen/constants/Weight.java
@@ -0,0 +1,19 @@
+package edu.ayd.joyfukitchen.constants;
+
+/**
+ * Created by Administrator on 2017/3/28.
+ * 重量单位枚举类
+ */
+
+public enum Weight {
+    //克
+    KE,
+    //安士（盎司)
+    ANSHI,
+    //两
+    LIANG,
+    //磅
+    BANG,
+    //毫升
+    HAOSHENG;
+}
