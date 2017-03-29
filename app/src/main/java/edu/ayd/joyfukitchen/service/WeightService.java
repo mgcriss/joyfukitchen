d256c36562dbbc5d0fa811aec6d6229811c887d0
535727655 <2877143504@qq.com>
2017-03-29 08:31:12 +0800

    add weightService

--- app/src/main/java/edu/ayd/joyfukitchen/service/WeightService.java
+++ app/src/main/java/edu/ayd/joyfukitchen/service/WeightService.java
@@ -8,4 +8,30 @@ package edu.ayd.joyfukitchen.service;
 public class WeightService {
 
     /**查询，*/
+
+
+    /**返回重量数据  单位:g */
+    public Float getWeight(){
+        return null;
+    }
+
+
+    /**
+     * 将重量转换成热量，需要参数:重量，食物种类
+     *
+     * */
+    public Float getEnerge(Float weight){
+        return null;
+    }
+
+    /**
+     * 保存称量数据
+     * @Param 需要参数:称量的
+     * */
+    public boolean saveRecord(){
+        return false;
+    }
+
+
+
 }
