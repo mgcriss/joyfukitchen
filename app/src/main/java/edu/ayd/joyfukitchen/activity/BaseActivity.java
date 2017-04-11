5e603e05769c625f15a1f32e5b885e4976941938
535727655 <2877143504@qq.com>
2017-04-11 09:05:14 +0800

    build.gradle添加字体框架

---
+++ app/src/main/java/edu/ayd/joyfukitchen/activity/BaseActivity.java
@@ -0,0 +1,18 @@
+package edu.ayd.joyfukitchen.activity;
+
+import android.app.Activity;
+import android.content.Context;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+
+public class BaseActivity extends Activity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        //初始化字体框架
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+}
